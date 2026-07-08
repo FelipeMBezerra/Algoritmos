c9cebe17ccc77484d4efdaa9dc849e3359da0354
FelipeMBezerra <107007542+FelipeMBezerra@users.noreply.github.com>
2026-07-08 14:04:55 -0300

    adiciona execício da sequência da Fibonacci

--- Aula 05/Lista03/exe15.ts
+++ Aula 05/Lista03/exe15.ts
@@ -2,3 +2,17 @@ import promptSync from "prompt-sync";
 const prompt = promptSync();
 
 //Desafio 15
+
+let p = 1; // primeiro
+let p1 = 1; // segundo
+let p2 = 1; // contador
+
+while (p2 <= 15){
+    console.log(p);
+
+    let p3 = p + p1; // proximo
+    p = p1;
+    p1 = p3;
+
+p2++;
+}
